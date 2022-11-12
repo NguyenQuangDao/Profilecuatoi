b2c2a87601e0ea4ba150dd1477a0943540aa7d1a
NguyenQuangDao <94677059+NguyenQuangDao@users.noreply.github.com>
2022-11-12 10:12:17 +0700

    ud

--- src/Contactme/Contactme.js
+++ src/Contactme/Contactme.js
@@ -12,37 +12,40 @@ function Contactme() {
     return (
         <div id="Contactme">
             <div className="title_Contactme">Contactme</div>
-            <div className="address_Contactme">
-                <div className="map_Contactme address">
-                    <FontAwesomeIcon icon="fa-solid fa-map-location-dot" id="map" className="icon_Contactme" />
-                    <span>
-                        Dan Phuong, Ha Noi city, Viet Nam
-                    </span>
+            <div className="background_Contactme">
+                <div className="address_Contactme">
+                    <div className="map_Contactme address">
+                        <FontAwesomeIcon icon="fa-solid fa-map-location-dot" id="map" className="icon_Contactme" />
+                        <span>
+                            Dan Phuong, Ha Noi city, Viet Nam
+                        </span>
+                    </div>
+                    <div className="mail_Contactme address">
+                        <FontAwesomeIcon icon="fa-solid fa-envelope-circle-check" id="mail" className="icon_Contactme" />
+                        <span>
+                            anhdao2292003cxz@gmail.com
+                        </span>
+                    </div>
+                    <div className="phone_Contactme address">
+                        <FontAwesomeIcon icon="fa-solid fa-phone" id="phone" className="icon_Contactme" />
+                        <span>
+                            0973 397 216
+                        </span>
+                    </div>
+                    <div className="clock_Contactme address">
+                        <FontAwesomeIcon icon="fa-solid fa-clock" id="clock" className="icon_Contactme" />
+                        <span>
+                            Mon - Fri 8:00 AM to 5:00 PM
+                        </span>
+                    </div>
                 </div>
-                <div className="mail_Contactme address">
-                    <FontAwesomeIcon icon="fa-solid fa-envelope-circle-check" id="mail" className="icon_Contactme" />
-                    <span>
-                        anhdao2292003cxz@gmail.com
-                    </span>
-                </div>
-                <div className="phone_Contactme address">
-                    <FontAwesomeIcon icon="fa-solid fa-phone" id="phone" className="icon_Contactme" />
-                    <span>
-                        0973 397 216
-                    </span>
-                </div>
-                <div className="clock_Contactme address">
-                    <FontAwesomeIcon icon="fa-solid fa-clock" id="clock" className="icon_Contactme" />
-                    <span>
-                        Mon - Fri 8:00 AM to 5:00 PM
-                    </span>
+
+                {/* <img id='background_Contactme' src={backGround_Contactme} /> */}
+                <div className="logo_Contactme">
+                    <img id="logoGDSC_logo_Contactme" src={logoGDSC} />
+                    <img id="logo_Contactme" src={logoSchool} />
                 </div>
             </div>
-            <div className="logo_Contactme">
-                <img id="logoGDSC_logo_Contactme" src={logoGDSC} />
-                <img id="logo_Contactme" src={logoSchool} />
-            </div>
-            <div className="background_Contactme"><img id='background_Contactme' src={backGround_Contactme} /></div>
         </div>
     )
 }
